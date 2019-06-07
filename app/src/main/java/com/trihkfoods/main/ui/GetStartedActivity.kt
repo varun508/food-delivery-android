f5baf55c1782b45c536ac25333ee5e688ad9c548
varun <imva508@gmail.com>
2019-06-08 04:18:39 +0530

    Added activity GetStarted

---
+++ app/src/main/java/com/trihkfoods/main/ui/GetStartedActivity.kt
@@ -0,0 +1,12 @@
+package com.trihkfoods.main.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GetStartedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+    }
+}
