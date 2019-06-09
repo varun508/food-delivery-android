a989c88dfd25302b10dfe6aedcebad35798a64cc
varun <imva508@gmail.com>
2019-06-09 21:28:23 +0530

    finish the onboarding screen

--- app/src/main/java/com/trihkfoods/main/ui/screens/activities/SplashActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/screens/activities/SplashActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashActivity : AppCompatActivity(){
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // navigate to the GetStarted activity after all the pre-processing is done
         startActivity(Intent(this, GetStartedActivity::class.java))
+        finish()
     }
 }
