5a9c2b667fc22abf38f01a68d1ca3dab31b1cf4d
varun <imva508@gmail.com>
2019-06-11 03:51:12 +0530

    Create the nav-graph-auth

--- app/src/main/java/com/trihkfoods/main/ui/screens/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/screens/authentication/AuthenticationActivity.kt
@@ -3,11 +3,14 @@ package com.trihkfoods.main.ui.screens.authentication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trihkfoods.main.R
+import com.trihkfoods.main.utils.changeStatusBarColor
 
 class AuthenticationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+
+        changeStatusBarColor(R.color.white)
     }
 }
