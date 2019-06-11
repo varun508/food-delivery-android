31922891caee5610d0e47623e970549edfad3c85
varun <imva508@gmail.com>
2019-06-11 16:05:38 +0530

    Created the ask phone number layout

--- app/src/main/java/com/trihkfoods/main/ui/screens/authentication/PhoneNumberFragment.kt
+++ app/src/main/java/com/trihkfoods/main/ui/screens/authentication/PhoneNumberFragment.kt
@@ -6,10 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.trihkfoods.main.R
+import com.trihkfoods.main.utils.scaleOnPress
+import kotlinx.android.synthetic.main.fragment_phone_number.*
 
 class PhoneNumberFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_phone_number, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvContinueFpn.scaleOnPress()
     }
 }
