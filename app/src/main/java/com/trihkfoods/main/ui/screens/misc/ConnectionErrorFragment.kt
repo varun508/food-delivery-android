6b3d8807ffae5ff5981bb264768b28e30fe9553c
varun508 <imva508@gmail.com>
2019-07-16 05:03:16 +0530

    Add no connection framgent

---
+++ app/src/main/java/com/trihkfoods/main/ui/screens/misc/ConnectionErrorFragment.kt
@@ -0,0 +1,19 @@
+package com.trihkfoods.main.ui.screens.misc
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.trihkfoods.main.R
+
+class ConnectionErrorFragment :Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_connection_error,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
