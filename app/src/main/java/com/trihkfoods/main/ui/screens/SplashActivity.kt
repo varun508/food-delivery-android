381a152c94e31a95fdc880dd4e36414112c430ec
varun508 <imva508@gmail.com>
2019-07-17 18:55:41 +0530

    Update the splash screen
    Added the trihk title and soem text on the splash screen and fixed the logo color and dimensions

--- app/src/main/java/com/trihkfoods/main/ui/screens/SplashActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/screens/SplashActivity.kt
@@ -2,12 +2,18 @@ package com.trihkfoods.main.ui.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.trihkfoods.main.R
 import com.trihkfoods.main.utils.navigateFinishing
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         // Navigate to the GetStarted activity after all the pre-processing is done
         navigateFinishing(GetStartedActivity::class.java)
