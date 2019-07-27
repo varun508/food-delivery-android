e47dd26b82ec40fe8534917fdfeaee81c03dcaab
varun508 <imva508@gmail.com>
2019-07-27 12:19:55 +0530

    Update chef item layout

--- app/src/main/java/com/trihkfoods/main/ui/adapters/MainChefSpecialListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/adapters/MainChefSpecialListAdapter.kt
@@ -1,32 +1,36 @@
 package com.trihkfoods.main.ui.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.trihkfoods.main.databinding.ListItemChefSpecialsMainBinding
 import com.trihkfoods.main.ui.tempmodels.FoodItem
 
 class MainChefSpecialListAdapter(private val items: List<FoodItem>) :
     RecyclerView.Adapter<MainChefSpecialListAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.from(parent)
 
-    }
-
-    override fun getItemCount(): Int {
-    }
+    override fun getItemCount() = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder(private val binding: ListItemChefSpecialsMainBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        companion object{
+        companion object {
 
-            fun from(parent: ViewGroup){
+            fun from(parent: ViewGroup): ViewHolder {
                 val inflater = LayoutInflater.from(parent.context)
-                val view = inflater.inflate(R.layout.)
+                val binding = ListItemChefSpecialsMainBinding.inflate(inflater, parent, false)
+                return ViewHolder(binding)
             }
+        }
+
+        fun bind(foodItem: FoodItem) {
+            binding.foodItem = foodItem
         }
     }
 }
