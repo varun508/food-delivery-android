d66d60d6c94cd25c6609018657e2bed37349add8
varun508 <imva508@gmail.com>
2019-07-27 10:00:10 +0530

    Add chef specials list

---
+++ app/src/main/java/com/trihkfoods/main/ui/adapters/MainChefSpecialListAdapter.kt
@@ -0,0 +1,19 @@
+package com.trihkfoods.main.ui.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainChefSpecialListAdapter : RecyclerView.Adapter<MainChefSpecialListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
