119ab16a62ca4c1f19e2f40e31c6ba8d0bc1714a
varun508 <imva508@gmail.com>
2019-07-28 12:08:41 +0530

    Update ExploreViewPagerAdapter

--- app/src/main/java/com/trihkfoods/main/ui/screens/main/explore/adapters/ExploreViewPagerAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/screens/main/explore/adapters/ExploreViewPagerAdapter.kt
@@ -2,10 +2,14 @@ package com.trihkfoods.main.ui.screens.main.explore.adapters
 
 import androidx.fragment.app.FragmentManager
 import com.trihkfoods.main.ui.adapters.BaseFragmentPagerAdapter
+import com.trihkfoods.main.ui.screens.main.explore.ChefExploreFragment
+import com.trihkfoods.main.ui.screens.main.explore.DishExploreFragment
 
-class ExploreFragmentPagerAdapter(fragmentManager: FragmentManager) : BaseFragmentPagerAdapter(fragmentManager) {
+class ExploreViewPagerAdapter(fragmentManager: FragmentManager) :
+    BaseFragmentPagerAdapter(fragmentManager) {
 
+    /** Use dependency injection to get instances of these fragments */
     init {
-        addFragments()   
+        addFragments(ChefExploreFragment(), DishExploreFragment())
     }
 }
