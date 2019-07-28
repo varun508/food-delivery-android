c5b5d8bedf1922331eb3b6976b14982d5a5baa2b
varun508 <imva508@gmail.com>
2019-07-28 17:08:03 +0530

    Add AccountOptionListAdapter

---
+++ app/src/main/java/com/trihkfoods/main/ui/screens/main/account/AccountOptionListAdapter.kt
@@ -0,0 +1,25 @@
+package com.trihkfoods.main.ui.screens.main.account
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AccountOptionListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    }
+
+    class ViewHolderHeader() : RecyclerView.ViewHolder() {
+
+    }
+
+    class ViewHolderOption() : RecyclerView.ViewHolder() {
+
+    }
+}
