811df6e5e6666cc3b9b09ea6fb5ca9ef403f763a
varun508 <imva508@gmail.com>
2019-08-04 18:22:23 +0530

    Add databinding to LocationActivity

--- app/src/main/java/com/trihkfoods/main/ui/location/LocationActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/location/LocationActivity.kt
@@ -2,14 +2,18 @@ package com.trihkfoods.main.ui.location
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.trihkfoods.main.R
+import com.trihkfoods.main.databinding.ActivityLocationBinding
 import com.trihkfoods.main.utils.changeStatusBarColor
 
 class LocationActivity : AppCompatActivity() {
 
+    private lateinit var _binding: ActivityLocationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_location)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_location)
         changeStatusBarColor(R.color.white)
     }
 }
