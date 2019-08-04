ad5bfe5e8b4d7ceebe9b15271417599d74438ba6
varun508 <imva508@gmail.com>
2019-08-04 23:44:32 +0530

    Update CouponListAdapter

--- app/src/main/java/com/trihkfoods/main/ui/coupon/CouponListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/coupon/CouponListAdapter.kt
@@ -1,5 +1,20 @@
 package com.trihkfoods.main.ui.coupon
 
-class CouponListAdapter {
-    
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.trihkfoods.main.databinding.ListItemApplyCouponBinding
+
+class CouponListAdapter : RecyclerView.Adapter<CouponListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    class ViewHolder(binding: ListItemApplyCouponBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
