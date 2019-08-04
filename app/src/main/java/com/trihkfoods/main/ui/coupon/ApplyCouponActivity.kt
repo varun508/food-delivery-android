0497d3e2f14b311d8d44e59efbbe02afc4c213e3
varun508 <imva508@gmail.com>
2019-08-04 22:37:56 +0530

    Add ApplyCouponActivity

---
+++ app/src/main/java/com/trihkfoods/main/ui/coupon/ApplyCouponActivity.kt
@@ -0,0 +1,12 @@
+package com.trihkfoods.main.ui.coupon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplyCouponActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apply_coupon)
+    }
+}
