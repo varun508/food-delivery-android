7ad4ae2c1c5bb9e2be69b1bf6122e8420e25893b
varun508 <imva508@gmail.com>
2019-08-04 23:48:15 +0530

    Make CouponListAdapter accept generic ViewHolder

--- app/src/main/java/com/trihkfoods/main/ui/coupon/CouponListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/coupon/CouponListAdapter.kt
@@ -2,9 +2,11 @@ package com.trihkfoods.main.ui.coupon
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.trihkfoods.main.databinding.ListItemApplyCouponBinding
 
-class CouponListAdapter : RecyclerView.Adapter<CouponListAdapter.ViewHolder>() {
+class CouponListAdapter(private val items: List<Any>) : RecyclerView.Adapter<ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
     }
@@ -15,6 +17,10 @@ class CouponListAdapter : RecyclerView.Adapter<CouponListAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
     }
 
-    class ViewHolder(binding: ListItemApplyCouponBinding) :
-        RecyclerView.ViewHolder(binding.root)
+    class ViewHolderCoupon(binding: ListItemApplyCouponBinding) :
+        ViewHolder(binding.root)
+
+
+    class ViewHolderAvailable(binding: ListItemApplyCouponBinding) :
+        ViewHolder(binding.root)
 }
