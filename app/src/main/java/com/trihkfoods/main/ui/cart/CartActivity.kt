11f6e400de794d9c5022b5c143a51abe880fa22a
varun508 <imva508@gmail.com>
2019-08-04 22:32:21 +0530

    Change cart activity status bar color

--- app/src/main/java/com/trihkfoods/main/ui/cart/CartActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/cart/CartActivity.kt
@@ -3,11 +3,13 @@ package com.trihkfoods.main.ui.cart
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trihkfoods.main.R
+import com.trihkfoods.main.utils.changeStatusBarColor
 
 class CartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        changeStatusBarColor(R.color.white)
     }
 }
