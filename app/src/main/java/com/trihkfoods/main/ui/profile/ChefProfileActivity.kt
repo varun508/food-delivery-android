0f75dd3d8ce4dead753f588bba3fa6a2b86f6029
varun508 <imva508@gmail.com>
2019-08-04 18:39:56 +0530

    Add ChefProfileactiivty

---
+++ app/src/main/java/com/trihkfoods/main/ui/profile/ChefProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.trihkfoods.main.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trihkfoods.main.R
+
+class ChefProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chef_profile)
+    }
+}
