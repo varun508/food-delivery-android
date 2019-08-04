7aae9b4ee11aa484afe035f17180df28f8b31207
varun508 <imva508@gmail.com>
2019-08-04 22:30:57 +0530

    Add cart activity

---
+++ app/src/main/java/com/trihkfoods/main/ui/CartActivity.kt
@@ -0,0 +1,13 @@
+package com.trihkfoods.main.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trihkfoods.main.R
+
+class CartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
