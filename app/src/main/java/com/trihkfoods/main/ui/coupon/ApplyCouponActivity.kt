363b7fc614a11c187f881d8715d944d6df11747f
varun508 <imva508@gmail.com>
2019-08-04 22:39:09 +0530

    Change status bar color of ApplyCouponActivity

--- app/src/main/java/com/trihkfoods/main/ui/coupon/ApplyCouponActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/coupon/ApplyCouponActivity.kt
@@ -1,12 +1,15 @@
 package com.trihkfoods.main.ui.coupon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.trihkfoods.main.R
+import com.trihkfoods.main.utils.changeStatusBarColor
 
 class ApplyCouponActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apply_coupon)
+        changeStatusBarColor(R.color.light_gray)
     }
 }
