b9bd52673a891a8af6a461b5c68c770ed40586aa
varun508 <imva508@gmail.com>
2019-08-04 22:33:56 +0530

    Add databinding to CartActivity

--- app/src/main/java/com/trihkfoods/main/ui/cart/CartActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/cart/CartActivity.kt
@@ -1,15 +1,19 @@
 package com.trihkfoods.main.ui.cart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.trihkfoods.main.R
+import com.trihkfoods.main.databinding.ActivityCartBinding
 import com.trihkfoods.main.utils.changeStatusBarColor
 
 class CartActivity : AppCompatActivity() {
 
+    private lateinit var _binding: ActivityCartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_cart)
         changeStatusBarColor(R.color.white)
     }
 }
