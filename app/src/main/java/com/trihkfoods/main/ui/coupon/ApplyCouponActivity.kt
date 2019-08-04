4f0651b56419ae282ff34d3169fa6044b7426083
varun508 <imva508@gmail.com>
2019-08-04 22:41:58 +0530

    Add databinding to ApplyCouponActivity

--- app/src/main/java/com/trihkfoods/main/ui/coupon/ApplyCouponActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/coupon/ApplyCouponActivity.kt
@@ -2,14 +2,19 @@ package com.trihkfoods.main.ui.coupon
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.trihkfoods.main.R
+import com.trihkfoods.main.databinding.ActivityApplyCouponBinding
 import com.trihkfoods.main.utils.changeStatusBarColor
 
 class ApplyCouponActivity : AppCompatActivity() {
 
+    private lateinit var _binding: ActivityApplyCouponBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_apply_coupon)
+
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_apply_coupon)
         changeStatusBarColor(R.color.light_gray)
     }
 }
