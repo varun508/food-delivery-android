aa9760d665f96ecf7add75208f720466a8bb3bb9
varun508 <imva508@gmail.com>
2019-08-04 17:46:16 +0530

    Add LocationActivity

---
+++ app/src/main/java/com/trihkfoods/main/ui/location/LocationActivity.kt
@@ -0,0 +1,13 @@
+package com.trihkfoods.main.ui.location
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trihkfoods.main.R
+
+class LocationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+    }
+}
