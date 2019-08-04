b364e4f106e2923144228f238fd7adc6df6ffa77
varun508 <imva508@gmail.com>
2019-08-04 23:57:43 +0530

    Update ViewHolderCoupon

--- app/src/main/java/com/trihkfoods/main/ui/coupon/CouponListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/coupon/CouponListAdapter.kt
@@ -1,5 +1,6 @@
 package com.trihkfoods.main.ui.coupon
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -18,29 +19,31 @@ class CouponListAdapter(private val items: List<Any>) : RecyclerView.Adapter<Vie
     }
 
     class ViewHolderCoupon(binding: ListItemApplyCouponBinding) :
-        ViewHolder(binding.root){
+        ViewHolder(binding.root) {
 
-        companion object{
-            fun  from(parent: ViewGroup){
-
+        companion object {
+            fun from(parent: ViewGroup): ViewHolderCoupon {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ListItemApplyCouponBinding.inflate(inflater, parent, false)
+                return ViewHolderCoupon(binding)
             }
         }
 
-        fun bind(){
+        fun bind() {
 
         }
     }
 
 
     class ViewHolderGenericHeader(itemView: View) :
-        ViewHolder(itemView){
-        companion object{
-            fun  from(parent: ViewGroup){
+        ViewHolder(itemView) {
+        companion object {
+            fun from(parent: ViewGroup) {
 
             }
         }
 
-        fun bind(){
+        fun bind() {
 
         }
     }
