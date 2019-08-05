46b30da88f1580c38e7fd69a5ad32650666daba5
varun508 <imva508@gmail.com>
2019-08-05 07:06:24 +0530

    Update list_item_payment_offer

--- app/src/main/java/com/trihkfoods/main/tempmodels/Coupon.kt
+++ app/src/main/java/com/trihkfoods/main/tempmodels/Coupon.kt
@@ -1,10 +1,10 @@
 package com.trihkfoods.main.tempmodels
 
 data class Coupon(
-    val id: String,
-    val couponCode: String,
-    val imageResource: Int,
-    val title: String,
-    val body: String,
-    val tnc: List<String>
+    val id: String = "",
+    val couponCode: String ="",
+    val imageResource: Int =0,
+    val title: String = "",
+    val body: String = "",
+    val tnc: List<String> = listOf()
 )
