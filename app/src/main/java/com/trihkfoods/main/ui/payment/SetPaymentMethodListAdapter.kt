96220dc84eba4b0436cbd8eaab20925aa57f067c
varun508 <imva508@gmail.com>
2019-08-07 02:05:08 +0530

    Add viewholders to SetPaymentMethodListAdapter

--- app/src/main/java/com/trihkfoods/main/ui/payment/SetPaymentMethodListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/payment/SetPaymentMethodListAdapter.kt
@@ -1,6 +1,33 @@
 package com.trihkfoods.main.ui.payment
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.trihkfoods.main.databinding.ListItemSavedPaymentMethodDualLineBinding
+import com.trihkfoods.main.databinding.ListItemSavedPaymentMethodSingleLineBinding
+
 class SetPaymentMethodListAdapter {
 
+
+    class ViewHolderSingleLine private constructor(binding: ListItemSavedPaymentMethodSingleLineBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun from(parent: ViewGroup) {
+
+            }
+        }
+
+    }
+
+    class ViewHolderDualLine private constructor(binding: ListItemSavedPaymentMethodDualLineBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun from(parent: ViewGroup) {
+
+            }
+        }
+
+    }
 
 }
