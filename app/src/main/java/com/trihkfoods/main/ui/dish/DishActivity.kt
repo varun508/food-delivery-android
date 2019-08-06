7764f1a2530107337fd845d929299ba7a36ba98a
varun508 <imva508@gmail.com>
2019-08-07 05:19:54 +0530

    Add DishActivity

---
+++ app/src/main/java/com/trihkfoods/main/ui/dish/DishActivity.kt
@@ -0,0 +1,12 @@
+package com.trihkfoods.main.ui.dish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dish)
+    }
+}
