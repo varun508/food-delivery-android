2328fea67441745375cd61931bb2fe08ce43ab1f
varun508 <imva508@gmail.com>
2019-08-06 12:14:45 +0530

    Fix crash with ItemCounterLayout

--- app/src/main/java/com/trihkfoods/main/widgets/ItemCounterLayout.kt
+++ app/src/main/java/com/trihkfoods/main/widgets/ItemCounterLayout.kt
@@ -7,8 +7,13 @@ import android.util.AttributeSet
 import android.widget.LinearLayout
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-class ItemCounterLayout(
-    context: Context, attributeSet: AttributeSet? = null, defStyle: Int = 0, defRes: Int = 0
+class ItemCounterLayout
+@JvmOverloads
+constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0,
+    defRes: Int = 0
 ) : LinearLayout(context, attributeSet, defStyle, defRes) {
 
     init {
