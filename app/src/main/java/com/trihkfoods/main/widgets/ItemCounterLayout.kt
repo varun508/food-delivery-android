67690e13311683b2f144b5811aafecd5bcabda27
varun508 <imva508@gmail.com>
2019-08-06 12:12:55 +0530

    Update ItemCounterLayout

--- app/src/main/java/com/trihkfoods/main/widgets/ItemCounterLayout.kt
+++ app/src/main/java/com/trihkfoods/main/widgets/ItemCounterLayout.kt
@@ -1,9 +1,17 @@
 package com.trihkfoods.main.widgets
 
+import android.annotation.TargetApi
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.widget.LinearLayout
 
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 class ItemCounterLayout(
-    context: Context, attributeSet: AttributeSet? = null, defStyle: Int = 0
-) : LinearLayout(context, attributeSet, defStyle)
+    context: Context, attributeSet: AttributeSet? = null, defStyle: Int = 0, defRes: Int = 0
+) : LinearLayout(context, attributeSet, defStyle, defRes) {
+
+    init {
+        orientation = HORIZONTAL
+    }
+}
