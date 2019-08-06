bd91a18b6b68fd80c0b0cb84a79b6a09e6df3bbb
varun508 <imva508@gmail.com>
2019-08-06 18:57:05 +0530

    Add

--- app/src/main/java/com/trihkfoods/main/ui/cart/CartChefListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/cart/CartChefListAdapter.kt
@@ -1,5 +1,11 @@
 package com.trihkfoods.main.ui.cart
 
+import androidx.recyclerview.widget.RecyclerView
+import com.trihkfoods.main.databinding.ListItemCartChefBinding
+
 class CartChefListAdapter {
 
+    class ViewHolder(binding:ListItemCartChefBinding): RecyclerView.ViewHolder(binding.root){
+
+    }
 }
--- app/src/main/java/com/trihkfoods/main/ui/cart/CartDishListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/cart/CartDishListAdapter.kt
@@ -1,5 +1,11 @@
 package com.trihkfoods.main.ui.cart
 
+import androidx.recyclerview.widget.RecyclerView
+import com.trihkfoods.main.databinding.ListItemCartDishBinding
+
 class CartDishListAdapter {
 
+    class ViewHolder(binding:ListItemCartDishBinding):RecyclerView.ViewHolder(binding.root){
+
+    }
 }
