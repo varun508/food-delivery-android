e7060ec86a632b301b8f0924b598713d315246ca
varun508 <imva508@gmail.com>
2019-08-06 21:01:25 +0530

    Add AddPaymentMethodActivity

---
+++ app/src/main/java/com/trihkfoods/main/ui/payment/AddPaymentMethodActivity.kt
@@ -0,0 +1,13 @@
+package com.trihkfoods.main.ui.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trihkfoods.main.R
+
+class AddPaymentMethodActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_payment_method)
+    }
+}
