cdba19db07e8e19461352e8273e14de20b312906
varun508 <imva508@gmail.com>
2019-08-06 21:03:58 +0530

    Add SetPaymentMethodActivity

---
+++ app/src/main/java/com/trihkfoods/main/ui/payment/SetPaymentMethodActivity.kt
@@ -0,0 +1,13 @@
+package com.trihkfoods.main.ui.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trihkfoods.main.R
+
+class SetPaymentMethodActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_payment_method)
+    }
+}
