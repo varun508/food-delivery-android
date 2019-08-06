3517d0e9043da67a01c0daae21e7f703f786a908
varun508 <imva508@gmail.com>
2019-08-06 19:04:50 +0530

    Update ViewHolders

--- app/src/main/java/com/trihkfoods/main/ui/cart/CartChefListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/cart/CartChefListAdapter.kt
@@ -1,11 +1,22 @@
 package com.trihkfoods.main.ui.cart
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.trihkfoods.main.R
 import com.trihkfoods.main.databinding.ListItemCartChefBinding
 
 class CartChefListAdapter {
 
     class ViewHolder(binding:ListItemCartChefBinding): RecyclerView.ViewHolder(binding.root){
 
+        companion object{
+
+            fun from(parent:ViewGroup):ViewHolder{
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ListItemCartChefBinding.inflate(inflater, parent,false)
+                return ViewHolder(binding)
+            }
+        }
     }
 }
--- app/src/main/java/com/trihkfoods/main/ui/cart/CartDishListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/cart/CartDishListAdapter.kt
@@ -1,11 +1,20 @@
 package com.trihkfoods.main.ui.cart
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.trihkfoods.main.databinding.ListItemCartDishBinding
 
 class CartDishListAdapter {
 
-    class ViewHolder(binding:ListItemCartDishBinding):RecyclerView.ViewHolder(binding.root){
+    class ViewHolder(binding: ListItemCartDishBinding) : RecyclerView.ViewHolder(binding.root) {
+        companion object {
 
+            fun from(parent: ViewGroup): ViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ListItemCartDishBinding.inflate(inflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
     }
 }
