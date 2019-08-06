77246c2856447aea06f45fa6848e71761b3fb064
varun508 <imva508@gmail.com>
2019-08-06 12:38:29 +0530

    Update ItemCounter widget

--- app/src/main/java/com/trihkfoods/main/widgets/ItemCounterLayout.kt
+++ app/src/main/java/com/trihkfoods/main/widgets/ItemCounterLayout.kt
@@ -4,9 +4,12 @@ import android.annotation.TargetApi
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import com.trihkfoods.main.R
+import kotlinx.android.synthetic.main.item_counter.view.*
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 class ItemCounterLayout
@@ -24,6 +27,26 @@ constructor(
         orientation = HORIZONTAL
 
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        inflater.inflate(R.layout.item_counter,this)
+        inflater.inflate(R.layout.item_counter, this)
+
+        tvItemCount.text = itemCount.toString()
+        background = ContextCompat.getDrawable(
+            context,
+            R.drawable.curved_rectangle_radius_regular_stroke_regular
+        )
+
+        setPadding(
+            getPixelValueFromDP(16f).toInt(),
+            getPixelValueFromDP(8f).toInt(),
+            getPixelValueFromDP(16f).toInt(),
+            getPixelValueFromDP(8f).toInt()
+        )
     }
+
+    private fun getPixelValueFromDP(value: Float) = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        value,
+        context.resources.displayMetrics
+    )
+
 }
