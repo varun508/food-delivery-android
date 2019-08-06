83558478bcf50305cb558bfc8845268629d8ff28
varun508 <imva508@gmail.com>
2019-08-06 19:11:53 +0530

    Update

--- app/src/main/java/com/trihkfoods/main/ui/cart/CartDishListAdapter.kt
+++ app/src/main/java/com/trihkfoods/main/ui/cart/CartDishListAdapter.kt
@@ -4,26 +4,33 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.trihkfoods.main.databinding.ListItemCartDishBinding
+import com.trihkfoods.main.tempmodels.FoodItem
 
-class CartDishListAdapter : RecyclerView.Adapter<CartDishListAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+class CartDishListAdapter(private val dishes: List<FoodItem>) :
+    RecyclerView.Adapter<CartDishListAdapter.ViewHolder>() {
 
-    }
 
-    override fun getItemCount(): Int {
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.from(parent)
+
+    override fun getItemCount() = dishes.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(dishes[position])
     }
 
+    class ViewHolder private constructor(private val binding: ListItemCartDishBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    class ViewHolder(binding: ListItemCartDishBinding) : RecyclerView.ViewHolder(binding.root) {
         companion object {
 
             fun from(parent: ViewGroup): ViewHolder {
                 val inflater = LayoutInflater.from(parent.context)
                 val binding = ListItemCartDishBinding.inflate(inflater, parent, false)
                 return ViewHolder(binding)
+            }
+        }
+
+        fun bind(dish: FoodItem) {
+            binding.root.run {
             }
         }
     }
