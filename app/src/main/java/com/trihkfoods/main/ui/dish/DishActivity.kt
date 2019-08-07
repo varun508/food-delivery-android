9a3f68bc2bad0d64f3a7f2a2f13dc28f481c8b8c
varun508 <imva508@gmail.com>
2019-08-07 05:44:12 +0530

    Add databinding to DishActivity

--- app/src/main/java/com/trihkfoods/main/ui/dish/DishActivity.kt
+++ app/src/main/java/com/trihkfoods/main/ui/dish/DishActivity.kt
@@ -1,13 +1,17 @@
 package com.trihkfoods.main.ui.dish
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.trihkfoods.main.R
+import com.trihkfoods.main.databinding.ActivityDishBinding
 
 class DishActivity : AppCompatActivity() {
 
+    private lateinit var _binding: ActivityDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dish)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_dish)
     }
 }
